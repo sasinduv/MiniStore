6d39be799a4cff5a81670327931ea417b8373c25
sasinduv <sasindugihan123456@gmail.com>
2026-03-09 22:15:16 +0530

    product list and search update

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -2,32 +2,30 @@ import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Product} from '../services/product';
 import {ProductArray} from '../types/ProductArray';
+import {ProductCard} from '../product-card/product-card';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [
+    ProductCard,
+    NgForOf
+  ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList implements OnInit{
 
-  productList:any[]=[];
-  constructor(private _service:Product) {
+  export class ProductList implements OnInit {
+  productList:any[]=[]; constructor(private service:Product) { } ngOnInit(): void { this.load(); }
+  load(){ this.service.findAllProducts().subscribe({ next:(res:any)=> { this.productList = res; console.log(this.productList) },
+    error:(err:any)=>{ console.log(err); }
+  })
   }
-    ngOnInit(): void {
-      this.load();
-    }
-    // load(){
-    //   this._service.findAllProducts().subscribe((response)=>{
-    //     this.productList.push(response);
-    //     console.log(this.productList)
-    //
-    //   })
-    // }
-  load() {
-    this._service.findAllProducts().subscribe((response: ProductArray[]) => {
-      this.productList = response;   // ✅ correct
-      console.log(this.productList);
-    });
 
+  search(event:any){
+    const value=event;
+  }
 }
+
+
+
